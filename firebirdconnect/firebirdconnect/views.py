882daa39290b70b13db9b682814418ec86fb8236
Getaante Yilma <GY@Getaantes-MacBook-Air.local>
2022-04-11 11:28:38 -0400

    changed templates in posts and groups app

--- firebirdconnect/firebirdconnect/views.py
+++ firebirdconnect/firebirdconnect/views.py
@@ -1,3 +1,5 @@
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView
 
 class TestPage(TemplateView):
@@ -7,5 +9,9 @@ class ThanksPage(TemplateView):
     template_name = 'thanks.html'
 
 class HomePage(TemplateView):
-    template_name = 'index.html'
-    
+    template_name = "index.html"
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated():
+            return HttpResponseRedirect(reverse("test"))
+        return super().get(request, *args, **kwargs)
--- firebirdconnect/groups/urls.py
+++ firebirdconnect/groups/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 app_name = 'groups'
-
+  
 urlpatterns = [
     url(r"^$", views.ListGroups.as_view(), name="all"),
     url(r"^new/$", views.CreateGroup.as_view(), name="create"),
